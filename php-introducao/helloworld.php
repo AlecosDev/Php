fca2d3d419742abf5419a507a7e04e44264da092
ALEX VINICIUS COSTA DA SILVA <20272488@CFP127ALUNO.LOCAL>
2021-02-24 11:30:59 -0300

    Atividade de backend do dia 24-02-2021

---
+++ php-introducao/helloworld.php
@@ -0,0 +1,9 @@
+<?php
+
+    // echo imprime strings no documento
+
+    echo "<h1> Hello World PHP! </h1>";
+
+    echo "<h2> Essa é minha primeira página PHP </h2>";
+
+?>
---
+++ php-introducao/variaveis.php
@@ -0,0 +1,13 @@
+<?php
+
+    const METAL_GEAR = "You're pretty good";
+
+    $nome = "Big Boss";
+
+    $anoDeNascimento = 1935;
+    
+    $idade =  date("Y") - $anoDeNascimento;
+
+    echo "Olá $nome, você tem $idade anos e nasceu em $anoDeNascimento";
+ 
+ 
